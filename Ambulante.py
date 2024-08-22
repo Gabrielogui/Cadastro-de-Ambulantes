3f4a3aaf6580d06dc354c42814f72845ab091cc1
Gabrielogui <gabrielcsro@gmail.com>
2024-08-22 17:12:03 -0300

    Criação das Classes Principai
    Classes:
    - Ambulantes e seus atributos
    - Ajudantes e seus atributos
    -> Não foi implementado nenhum método

---
+++ Ambulante.py
@@ -0,0 +1,30 @@
+# ======= CLASSE AMBULANTE =======
+class Ambulante():
+    # ======= CONSTRUTOR =======
+    def __init__(self):
+        self.nome         = None
+        self.cpf          = None
+        self.raca         = None
+        self.municipio    = None
+        self.bairro       = None
+        self.rua          = None
+        self.ddd_celular  = None
+        self.celular      = None
+        self.ddd_whatsapp = None
+        self.whatsapp     = None
+        self.rg           = None
+        self.escolaridade = None
+        self.atividade_equipamento = None
+        self.ajudante     = None
+        self.autoriza_pesquisa = None
+        self.aceito       = None
+        self.hora_atualizacao = None
+
+    # ======= MÉTODOS =======
+
+# ======= CLASSE AJUDANTE =======
+class Ajudante():
+    def __init__(self):
+        self.nome = None
+        self.cpf  = None
+
