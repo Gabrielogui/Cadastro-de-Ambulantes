69489e0ea1ecf61b0b8deaec2bc61272f36ad017
Gabrielogui <gabrielcsro@gmail.com>
2024-11-13 07:38:05 -0300

    Controle + Ambulante att
     - Ambulante: Todos os atributos certos
     - controle: Criação/finalização da criação das tabelas no banco de dados + criação dos métodos CRUD que serão finalizados depois

--- src/controle/controle.py
+++ src/controle/controle.py
@@ -20,15 +20,18 @@ class Controle():
         self.nomeMae_entry.delete(0, tk.END)
         self.dataNascimento_entry.delete(0, tk.END)
         
+    # ======= CONECTANDO COM O BANCO DE DADOS =======
     def conecta_bd(self):
         self.conn = sqlite3.connect('cadastro_ambulante.bd')
         self.cursor = self.conn.cursor()
         print('conectando ao banco de dados !')
 
+    # ======= DESCONECTANDO COM O BANCO DE DADOS =======
     def desconecta_bd(self):
         self.conn.close()
         print('desconectando do banco de dados!')
 
+    # ======= MONTANDO AS TABELAS NECESSÁRIAS PARA O BANCO DE DADOS =======
     def montar_tabelas(self):
         self.conecta_bd()
 
@@ -36,6 +39,62 @@ class Controle():
             CREATE TABLE IF NOT EXISTS ambulante(
                 id INTEGER PRIMARY KEY,
                 nome char(50) NOT NULL,
-                
-                )
+                cpf char(11) NOT NULL,
+                rg char(10) NOT NULL,
+                email char(30) NOT NULL,
+                telefone INTEGER(20),
+                cep char(8) NOT NULL,
+                cidade char(30) NOT NULL,
+                bairro char(30) NOT NULL,
+                rua char(30) NOT NULL,
+                atividade char(30) NOT NULL,
+                data_nascimento date NOT NULL,
+                nome_mae char(50) NOT NULL,
+                raca char(30) NOT NULL,
+                genero char(30) NOT NULL,
+                deficiencia char(3) NOT NULL,
+                escolaridade char(30) NOT NULL,
+                trabalha char(3) NOT NULL,
+                faixa_salarial char(10),
+                hora_atualizacao date
+            )
+                            
+            CREATE TABLE IF NOT EXISTS ajudante(
+                id_ajudante INTEGER PRIMARY KEY NOT NULL,
+                nome_ajudante char(50) NOT NULL,
+                cpf_ajudante char(11) NOT NULL,
+                data_nascimento date NOT NULL,
+                id_ambulante INTEGER FOREIGN KEY NOT NULL
+            )
         ''')
+
+        self.conn.commit()
+        print('Banco de dados Criado!')
+        self.desconecta_bd()
+
+    # |=======| MÉTODOS 'CRUD' |=======|
+    # ======= MÉTODO DE CADASTRAR O AMBULANTE =======
+    def cadastrarAmbulante():
+        pass
+
+    # ======= MÉTODO DE VISUALIZAR O AMBULANTE NA LISTA (FRAME 03) - TREEVIEW =======
+    def visualizarListaAmbulante():
+        pass
+
+    # ======= MÉTODO DE REMOVER O AMBULANTE =======
+    def removerAmbulante():
+        pass
+
+    # ======= METODO PARA ATUALIZAR O AMBULANTE =======
+    def atualizarAmbulante():
+        pass
+    
+    # ======= MÉTODO PARA PEGAR O AMBULANTE E SUBI-LO AO FRAME 02 QUANDO FOR CLICADO 2X =======
+    def onDoubleClick():
+        pass
+
+    # ======= MÉTODO DE BUSCA DO AMBULANTE(NOME, CPF, ID) =======
+    def buscarAmbulante():
+        pass
+
+
--- src/modelo/Ambulante.py
+++ src/modelo/Ambulante.py
@@ -2,26 +2,34 @@
 class Ambulante():
     # ======= CONSTRUTOR =======
     def __init__(self):
-        self.id           = None
-        self.nome         = None
-        self.cpf          = None
-        self.email        = None
-        self.raca         = None
-        self.cep          = None
-        self.cidade       = None
-        self.bairro       = None
-        self.rua          = None
+        self.id              = None
+        self.nome            = None
+        self.cpf             = None
+        self.rg              = None
+        self.email           = None
+        self.telefone        = None
+        self.cep             = None
+        self.cidade          = None
+        self.bairro          = None
+        self.rua             = None
+        self.atividade       = None
+        self.data_nascimento = None
+        self.nome_mae        = None
+        self.raca            = None
+        self.genero          = None
+        self.deficiencia     = None
+        self.escolaridade    = None
+        self.ajudantes       = []
+        self.trabalha        = None
+        self.faixaSalarial   = None
+
+        # PEGAR NA HORA QUE FOR INSERIDO:
+        self.hora_atualizacao = None
+
+        # PEGAR AUTOMÁTICO - TELEFONE:
         self.ddd_celular  = None
-        self.celular      = None
         self.ddd_whatsapp = None
         self.whatsapp     = None
-        self.rg           = None
-        self.escolaridade = None
-        self.atividade_equipamento = None
-        self.ajudante     = None
-        self.autoriza_pesquisa = None
-        self.aceito       = None
-        self.hora_atualizacao = None
 
     # ======= MÉTODOS =======
 
@@ -31,4 +39,7 @@ class Ajudante():
         self.nome           = None
         self.cpf            = None
         self.dataNascimento = None
+
+        # AMBULANTE VINCULADO
+        self.idAmbulante    = None
 
